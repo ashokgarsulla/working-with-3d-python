1981e25f06d851d1f971d2b4230ff8f44e86b488
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-11-29 16:41:00 +0530

    all points coordinate added

--- Base/Base.py
+++ Base/Base.py
@@ -43,6 +43,56 @@ class BaseWindow(QMainWindow):
         self.ren = vtk.vtkRenderer()
         self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
 
+        # points
+        points = {
+            1:[0,0,0], 
+            2:[0,14.44,0], 
+            3:[14.44,0,0], 
+            4:[0,28.89,0], 
+            5:[28.88,0,0], 
+            6:[0,43.33,0], 
+            7:[43.32,0,0], 
+            8:[0,57.78,0], 
+            9:[57.76,0,0], 
+            10:[0,72.22,0], 
+            11:[72.20,0,0], 
+            12:[0,86.66,0], 
+            13:[86.64,0,0], 
+            14:[0,101.11,0],
+            15:[101.08,0,0], 
+            16:[0,115.55,0], 
+            17:[115.52,0,0], 
+            18:[0,130.00,0], 
+            19:[129.96,0,0], 
+            20:[32.50,32.50,0], 
+            21:[16.25,113.75,0],
+            22:[32.50,97.50,0], 
+            23:[48.74,81.25,0], 
+            24:[64.99,65.01], 
+            25:[81.24,48.76,0], 
+            26:[97.49,32.51,0], 
+            27:[113.73,16.26,0], 
+            28:[35.36,130.00,10.28],
+            29:[59.34,115.91,10.28], 
+            30:[68.54,96.82,10.28], 
+            31:[87.63,87.63,15.28], 
+            32:[96.82,68.54,15.28], 
+            33:[115.91,59.34,15.28], 
+            34:[130.00,35.36,15.28], 
+            35:[65.87,129.99,36.62],
+            36:[97.94,97.94,36.63], 
+            37:[130.00,65.87,65.87], 
+            38:[88.44,116.72,43.20], 
+            39:[116.72,88.44,43.20], 
+            40:[84.43,130.01,49.75], 
+            41:[107.23,107.23,49.77], 
+            42:[130.01,84.43,49.75],
+            43:[101.89,130.00,56.35], 
+            44:[130.00,101.89,56.35], 
+            45:[120.18,120.18,56.35], 
+            46:[118.81,130.00,56.35], 
+            47:[130.00,118.81,56.35] 
+        }
         self.colors = vtkNamedColors()
 
         self.filename = "AccuracyTestingJigMainBody.stl"
@@ -54,15 +104,15 @@ class BaseWindow(QMainWindow):
         self.mapper.SetInputConnection(self.reader.GetOutputPort())
 
         self.targetSphere = vtk.vtkSphereSource()
-        self.targetSphere.SetRadius(50.0)
+        self.targetSphere.SetRadius(5.0)
         self.targetSphereMapper = vtk.vtkPolyDataMapper()
         self.targetSphereMapper.SetInputConnection(self.targetSphere.GetOutputPort())
         self.targetSphereActor = vtk.vtkActor()
         self.targetSphereActor.SetMapper(self.targetSphereMapper)
         self.targetSphereActor.GetProperty().SetColor(self.colors.GetColor3d("red"))
         # self.SetTargetSphereScale(3)
-        self.targetSphereActor.SetScale(0.1, 0.1, 0.1)
-        self.targetSphereActor.SetPosition(22, 22, 22)
+        # self.targetSphereActor.SetScale(0.1, 0.1, 0.1)
+        self.targetSphereActor.SetPosition(points[44][0],points[44][1],points[44][2])
 
         self.phantom = vtkActor()
         self.phantom.SetMapper(self.mapper)
@@ -70,10 +120,11 @@ class BaseWindow(QMainWindow):
         self.phantom.GetProperty().SetDiffuseColor(self.colors.GetColor3d('LightSteelBlue'))
         self.phantom.GetProperty().SetSpecular(0.3)
         self.phantom.GetProperty().SetSpecularPower(60.0)
-        self.phantom.SetScale(1, 1, 1)
+        self.phantom.SetPosition(0,0,0)
+        # self.phantom.SetScale(1, 1, 1)
 
         # Probe
-        self.probefilename = "holecube.stl"
+        self.probefilename = "HandheldProbeStemLong.stl"
 
         self.probereader = vtkSTLReader()
         self.probereader.SetFileName(self.probefilename)
@@ -87,9 +138,9 @@ class BaseWindow(QMainWindow):
         self.probeActor.GetProperty().SetDiffuseColor(self.colors.GetColor3d('LightSteelBlue'))
         self.probeActor.GetProperty().SetSpecular(0.3)
         self.probeActor.GetProperty().SetSpecularPower(60.0)
-        self.probeActor.SetScale(1.2, 1.2, 1.2)
+        self.probeActor.SetPosition(0,0,0)
 
-        self.SetProbePosition(120,120,120)
+        # self.SetProbePosition(90,90,90)
         self.SetProbeVisibility(True)
 
         self.ren.AddActor(self.targetSphereActor)
